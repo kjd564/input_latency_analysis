694f1a81ae18710a3234806e5699ed48d54d4ca8
kjd564 <kdillon@chromium.org>
2019-04-04 16:06:33 -0700

    new data, small updates

--- utils.js
+++ utils.js
@@ -104,7 +104,7 @@ function toggleDataType(input_event, chart, normalize) {
     chart.config.options.scales.xAxes[0].scaleLabel.labelString = 'duration (%)';
   } else {
     chart.config.data = GRAPH_DATA[input_event].raw;
-    chart.config.options.scales.xAxes[0].ticks.max = 1400
+    chart.config.options.scales.xAxes[0].ticks.max = 1200
     chart.config.options.scales.xAxes[0].scaleLabel.labelString = 'duration (ms)';
   }
   chart.update();
@@ -174,7 +174,7 @@ function createChart(input_event, title, event_data) {
 	xAxes: [{
 	  stacked: true,
 	  ticks: {
-	    max: 1400,
+	    max: 1200,
 	    fontSize: 20,
 	  },
 	  scaleLabel: {
